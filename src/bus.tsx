8495a1ef5b193da8dd16448be31dc38223b0e247
Joseph B <33590262+jbelian@users.noreply.github.com>
2023-09-29 19:04:38 -0500

    Simplified code in bus.tsx

--- src/bus.tsx
+++ src/bus.tsx
@@ -14,63 +14,51 @@ const BusDataDisplay: React.FC = () => {
   const [selectedDate1, setSelectedDate1] = useState<string>("2001-01");
   const [selectedDate2, setSelectedDate2] = useState<string>("2002-01");
 
-  const filteredData1 = busDataArray.filter((item) => {
-    const itemMonth = item.month_beginning.substring(0, 7);
-    return itemMonth === selectedDate1;
-  });
+  const returnSelectedDate = (selectedDate: string) => {
+    return busDataArray.filter((item) => {
+      const itemMonth = item.month_beginning.substring(0, 7);
+      return itemMonth === selectedDate;
+    });
+  };
 
-  const filteredData2 = busDataArray.filter((item) => {
-    const itemMonth = item.month_beginning.substring(0, 7);
-    return itemMonth === selectedDate2;
-  });
+  const filteredData1 = returnSelectedDate(selectedDate1);
+  const filteredData2 = returnSelectedDate(selectedDate2);
 
-  const combinedFilteredData: RouteData[] = [];
+  interface extendedRouteData extends RouteData {
+    monthtotal2: String;
+  }
+  const combinedFilteredData: extendedRouteData[] = [];
 
   filteredData1.forEach((item1) => {
     const matchingItem2 = filteredData2.find(
       (item2) => item2.route === item1.route
     );
-    if (matchingItem2) {
-      combinedFilteredData.push({
-        route: item1.route,
-        routename: item1.routename,
-        month_beginning: item1.month_beginning,
-        monthtotal1: item1.monthtotal,
-        monthtotal2: matchingItem2.monthtotal,
-      });
-    } else {
-      combinedFilteredData.push({
-        route: item1.route,
-        routename: item1.routename,
-        month_beginning: item1.month_beginning,
-        monthtotal1: item1.monthtotal,
-        monthtotal2: "",
-      });
-    }
+    combinedFilteredData.push({
+      route: item1.route,
+      routename: item1.routename,
+      month_beginning: item1.month_beginning,
+      monthtotal: item1.monthtotal,
+      monthtotal2: matchingItem2 ? matchingItem2.monthtotal : "",
+    });
   });
 
-  // Add any items from filteredData2 that do not have a match in filteredData1
   filteredData2.forEach((item2) => {
-    const matchingItem1 = filteredData1.find(
-      (item1) => item1.route === item2.route
-    );
-    if (!matchingItem1) {
+    if (!filteredData1.find((item1) => item1.route === item2.route)) {
       combinedFilteredData.push({
         route: item2.route,
         routename: item2.routename,
         month_beginning: item2.month_beginning,
-        monthtotal1: "",
+        monthtotal: "",
         monthtotal2: item2.monthtotal,
       });
     }
   });
 
-  const handleDate1Change = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSelectedDate1(event.target.value);
-  };
-
-  const handleDate2Change = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSelectedDate2(event.target.value);
+  const handleDateChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    setSelectedDate: React.Dispatch<React.SetStateAction<string>>
+  ) => {
+    setSelectedDate(event.target.value);
   };
 
   return (
@@ -81,14 +69,14 @@ const BusDataDisplay: React.FC = () => {
         <input
           type="month"
           value={selectedDate1}
-          onChange={handleDate1Change}
+          onChange={(event) => handleDateChange(event, setSelectedDate1)}
         />
       </div>
       <div>
         <input
           type="month"
           value={selectedDate2}
-          onChange={handleDate2Change}
+          onChange={(event) => handleDateChange(event, setSelectedDate2)}
         />
       </div>
 
@@ -98,20 +86,22 @@ const BusDataDisplay: React.FC = () => {
             <th>Route</th>
             <th>Route Name</th>
             {/* <th>Month Beginning</th> */}
-            <th>Month Total 1</th>
-            <th>Month Total 2</th>
+            <th>Monthly Ridership 1</th>
+            <th>Monthly Ridership 2</th>
           </tr>
         </thead>
         <tbody>
-          {combinedFilteredData.map((item: RouteData, index: number) => (
-            <tr key={index}>
-              <td>{item.route}</td>
-              <td>{item.routename}</td>
-              {/* <td>{item.month_beginning}</td> */}
-              <td>{item.monthtotal1}</td>
-              <td>{item.monthtotal2}</td>
-            </tr>
-          ))}
+          {combinedFilteredData.map(
+            (item: extendedRouteData, index: number) => (
+              <tr key={index}>
+                <td>{item.route}</td>
+                <td>{item.routename}</td>
+                {/* <td>{item.month_beginning}</td> */}
+                <td>{item.monthtotal}</td>
+                <td>{item.monthtotal2}</td>
+              </tr>
+            )
+          )}
         </tbody>
       </table>
     </div>
