e359c14b3e621c00277bb02a205c081eba3ff0fd
Joseph B <33590262+jbelian@users.noreply.github.com>
2023-11-07 13:32:37 -0600

    added popup for each busline clicked on map

--- src/map.tsx
+++ src/map.tsx
@@ -5,16 +5,20 @@ import "./App.css";
 import { jawgToken } from "./token.tsx";
 import doc from "./data/doc.json";
 
-const docData = doc as FeatureCollection;
+function busLinePopup(feature: any, layer: any) {
+  console.log(feature);
+  layer.bindPopup(`This is the ${feature.properties.Name} bus line`);
+}
 
 export default function Map() {
+  const docData = doc as FeatureCollection;
   return (
     <MapContainer className="map" center={[41.8781, -87.63]} zoom={13}>
       <TileLayer
         attribution='<a href="http://jawg.io" title="Tiles Courtesy of Jawg Maps" target="_blank">&copy; <b>Jawg</b>Maps</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url={`https://{s}.tile.jawg.io/jawg-dark/{z}/{x}/{y}{r}.png?access-token=${jawgToken}`}
       />
-      <GeoJSON data={docData} />
+      <GeoJSON data={docData} onEachFeature={busLinePopup} />
     </MapContainer>
   );
 }
