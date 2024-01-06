022883d5676d7daa886bb01f669192d7538be01c
Joseph <33590262+jbelian@users.noreply.github.com>
2024-01-06 03:43:11 -0500

    updated API

--- .github/workflows/access_api.py
+++ .github/workflows/access_api.py
@@ -1,11 +1,32 @@
 import os
 import json
+import requests
 from sodapy import Socrata
 
 domain = "data.cityofchicago.org"
+resource = "bynn-gwxy"
 token = os.getenv('SOCRATA_TOKEN')
 client = Socrata(domain, token)
 bus_data = client.get("bynn-gwxy", limit=50000)
+
+last_modified_path = 'src/data/last_modified.txt'
+bus_data_path = 'src/data/bus_data.json'
+
+try:
+    with open(last_modified_path, 'r') as f:
+        last_modified = f.read().strip()
+except FileNotFoundError:
+    last_modified = None
+
+response = requests.head(f"https://{domain}/resource/{resource}.json",
+                         headers={"X-App-Token": token})
+
+if last_modified != response.headers.get('Last-Modified'):
+    bus_data = client.get(resource, limit=50000)
+    with open(bus_data_path, 'w') as f:
+        json.dump(bus_data, f)
+    with open(bus_data_path, 'w') as f:
+        f.write(response.headers.get('Last-Modified'))
 
 with open('src/data/bus_data.json', 'w') as f:
     json.dump(bus_data, f)
