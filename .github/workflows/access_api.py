a944247307c15711ebaba7bbbe4d497f05059c83
Joseph <33590262+jbelian@users.noreply.github.com>
2024-01-06 03:03:04 -0500

    updated API

--- .github/workflows/access_api.py
+++ .github/workflows/access_api.py
@@ -5,7 +5,7 @@ from sodapy import Socrata
 domain = "data.cityofchicago.org"
 token = os.getenv('SOCRATA_TOKEN')
 client = Socrata(domain, token)
-results = client.get("bynn-gwxy", limit=50000)
+bus_data = client.get("bynn-gwxy", limit=50000)
 
-with open('results.json', 'w') as f:
-    json.dump(results, f)
+with open('src/data/bus_data.json', 'w') as f:
+    json.dump(bus_data, f)
