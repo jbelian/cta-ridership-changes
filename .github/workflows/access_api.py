b2995f0fac753d127a0c8ba0cb24ee9b185e052a
Joseph <33590262+jbelian@users.noreply.github.com>
2024-01-08 03:49:49 -0500

    updated API

--- .github/workflows/access_api.py
+++ .github/workflows/access_api.py
@@ -7,14 +7,9 @@ domain = "data.cityofchicago.org"
 resource = "bynn-gwxy"
 token = os.getenv('SOCRATA_TOKEN')
 client = Socrata(domain, token)
-limit = 3
-
-last_modified_path = 'src/data/last_modified.txt'
-bus_data_path = 'src/data/busData.json'
-bus_data_dir = 'src/data'
 
 try:
-    with open(last_modified_path, 'r') as f:
+    with open('src/data/last_modified.txt', 'r') as f:
         last_modified = f.read().strip()
 except FileNotFoundError:
     last_modified = None
@@ -26,12 +21,12 @@ print(response)
 print(response.headers)
 
 if last_modified != response.headers.get('Last-Modified'):
-    busData = client.get(resource, limit=limit)
+    busData = client.get(resource, limit=100000)
 
-    if not os.path.exists(bus_data_dir):
-        os.makedirs(bus_data_dir)
+    if not os.path.exists('src/data'):
+        os.makedirs('src/data')
 
-    with open(bus_data_path, 'w') as f:
+    with open('src/data/busData.json', 'w') as f:
         json.dump(busData, f)
-    with open(last_modified_path, 'w') as f:
+    with open('src/data/last_modified.txt', 'w') as f:
         f.write(response.headers.get('Last-Modified')  or '')
