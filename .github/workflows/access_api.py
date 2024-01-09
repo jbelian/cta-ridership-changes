3f2c876e09a8c065b6989dcca6d9ec91d276c31d
Joseph <33590262+jbelian@users.noreply.github.com>
2024-01-09 05:45:37 -0500

    updated API call

--- .github/workflows/access_api.py
+++ .github/workflows/access_api.py
@@ -3,30 +3,25 @@ import json
 import requests
 from sodapy import Socrata
 
+os.makedirs('src/data', exist_ok=True)
 domain = "data.cityofchicago.org"
-resource = "bynn-gwxy"
 token = os.getenv('SOCRATA_TOKEN')
 client = Socrata(domain, token)
+
+response = requests.get(f"https://{domain}/resource/bynn-gwxy.json",
+                         headers={"X-App-Token": token})
+
+print(response)
+print(response.headers)
 
 try:
     with open('src/data/last_modified.txt', 'r') as f:
         last_modified = f.read().strip()
 except FileNotFoundError:
     last_modified = None
-
-response = requests.get(f"https://{domain}/resource/{resource}.json",
-                         headers={"X-App-Token": token})
-
-print(response)
-print(response.headers)
-
 if last_modified != response.headers.get('Last-Modified'):
-    busData = client.get(resource, limit=100000)
 
-    if not os.path.exists('src/data'):
-        os.makedirs('src/data')
-
-    with open('src/data/busData.json', 'w') as f:
-        json.dump(busData, f)
+    with open('src/data/bus.json', 'w') as f:
+        json.dump(client.get('bynn-gwxy', limit=100000), f)
     with open('src/data/last_modified.txt', 'w') as f:
-        f.write(response.headers.get('Last-Modified')  or '')
+        f.write(response.headers.get('Last-Modified') or '')
---
+++ .github/workflows/kmz_to_json.py
@@ -0,0 +1,19 @@
+import requests
+import zipfile
+from osgeo import ogr
+
+response = requests.get(f"https://data.cityofchicago.org/download/rytz-fq6y/"
+                            "application%2Fvnd.google-earth.kmz")
+
+with open('temp.kmz', 'wb') as f:
+    f.write(response.content)
+kmz = zipfile.ZipFile('temp.kmz', 'r')
+kml = kmz.open('doc.kml', 'r').read()
+with open('temp.kml', 'wb') as f:
+    f.write(kml)
+kml_driver = ogr.GetDriverByName('KML')
+kml_ds = kml_driver.Open('temp.kml')
+
+# Though GeoJSON, the file extension must be .json for later manipulation
+geojson_driver = ogr.GetDriverByName('GeoJSON')
+geojson_driver.CopyDataSource(kml_ds, 'src/data/map.json')
