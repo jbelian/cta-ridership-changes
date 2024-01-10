cdd3c2f08efaa77006a5649a1531b14679f8513c
Joseph <33590262+jbelian@users.noreply.github.com>
2024-01-10 03:26:46 -0500

    end date now fetched from API call and not hard coded

--- src/busDisplay.tsx
+++ src/busDisplay.tsx
@@ -1,6 +1,6 @@
 // busDisplay.tsx
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { CombinedRoutes } from "./busData.tsx";
 
 interface RouteSelectionProps {
@@ -20,6 +20,20 @@ function RouteSelection(props: RouteSelectionProps) {
     filteredRoutes
   } = props;
 
+  // Displaying the datetime of the last time the data was fetched
+  // The most recent month of data is used as the end date for the date selector
+  const [lastMonth, setLastMonth] = useState('');
+  const [lastFetched, setLastFetched] = useState('');
+  useEffect(() => {
+    fetch('../data/last_modified.txt')
+      .then(response => response.text())
+      .then(data => {
+        const lines = data.split('\n').map(line => line.replace('\r', ''));
+        setLastMonth(lines[1].slice(-7));
+        setLastFetched(lines[2]);
+      });
+  }, []);
+
   const getYearAndMonthName = (dateString: string) => {
     const date = new Date(`${dateString}T00:00:00`);
     const year = date.getFullYear();
@@ -28,17 +42,19 @@ function RouteSelection(props: RouteSelectionProps) {
     }).format(date);
     return `${monthName} ${year}`;
   };
+
   return (
     <div>
+      <pre>{lastFetched}</pre>
       <label>
-        Select years and months to compare
+        Select years and months to compare:
         {[selectedDate1, selectedDate2].map((selectedDate, index) => (
           <div key={index}>
             <input
               name="date"
               type="month"
               min="2001-01"
-              max="2023-06"
+              max={lastMonth}
               value={selectedDate}
               onChange={(event) =>
                 index === 0 ? onDateChange1(event) : onDateChange2(event)
