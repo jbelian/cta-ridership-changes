330d306b3eed26715754942e0efa0d2d1a1726e3
Joseph <33590262+jbelian@users.noreply.github.com>
2024-01-09 22:04:30 -0500

    updated API call

--- .github/workflows/access_api.py
+++ .github/workflows/access_api.py
@@ -3,11 +3,11 @@ import json
 import requests
 from sodapy import Socrata
 
+# API info for bus ridership data
 os.makedirs('data', exist_ok=True)
 domain = "data.cityofchicago.org"
 token = os.getenv('SOCRATA_TOKEN')
 client = Socrata(domain, token)
-
 response = requests.get(f"https://{domain}/resource/bynn-gwxy.json",
                          headers={"X-App-Token": token})
 
@@ -16,19 +16,24 @@ print(response.headers)
 
 try:
     with open('data/last_modified.txt', 'r') as f:
-        last_modified = f.read().strip()
+        last_modified = f.readline().replace("Last modified: ", "").strip()
 except FileNotFoundError:
     last_modified = None
 
+# If the header's 'Last-Modified' value hasn't changed, then this workflow ends here
 response_lm = response.headers.get('Last-Modified')
 if last_modified != response_lm:
     last_modified = response_lm
-    
+
+    # Download the bus ridership data    
     data = client.get('bynn-gwxy', limit=100000)
     with open('data/bus.json', 'w') as f:
         json.dump(data, f)
 
+    # The most recent month will be used as the end date in the date selector
     last_month = max([item['month_beginning'][:7] for item in data])
+    last_fetch = response.headers.get('Date')
     with open('data/last_modified.txt', 'w') as f:
-        f.write((f"{last_modified or ''}"
-                 f"\n{last_month}"))
+        f.write((f"Last modified: {last_modified or ''}"
+                 f"\nLast month with data: {last_month}"
+                 f"\nLast fetched:  {last_fetch or ''}"))
