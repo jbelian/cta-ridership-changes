910e7f237c64847ece5baa32a49bb1b1c0fbaa31
Joseph <33590262+jbelian@users.noreply.github.com>
2024-01-11 22:33:35 -0500

    update

--- .github/workflows/kmz_to_json.py
+++ .github/workflows/kmz_to_json.py
@@ -1,6 +1,8 @@
 import requests
 import zipfile
 import subprocess
+from bs4 import BeautifulSoup
+import json
 
 response = requests.get(f"https://data.cityofchicago.org/download/rytz-fq6y/"
                             "application%2Fvnd.google-earth.kmz")
@@ -14,3 +16,16 @@ with open('temp.kml', 'wb') as f:
 
 # Use ogr2ogr to convert KML to GeoJSON
 subprocess.run(['ogr2ogr', '-f', 'GeoJSON', 'data/map.json', 'temp.kml'])
+
+# Load the GeoJSON file
+with open('data/map.json', 'r') as f:
+    data = json.load(f)
+
+# Iterate over the features and clean the HTML from the 'description' field
+for feature in data['features']:
+    soup = BeautifulSoup(feature['properties']['description'], 'html.parser')
+    feature['properties']['description'] = soup.get_text()
+
+# Write the cleaned data back to the file
+with open('data/map.json', 'w') as f:
+    json.dump(data, f)
