fe7637a088a1f0c8e3b8999dd4f9f08710b4ed3d
Joseph <33590262+jbelian@users.noreply.github.com>
2024-01-11 23:42:52 -0500

    up

--- .github/workflows/kmz_to_json.py
+++ .github/workflows/kmz_to_json.py
@@ -4,9 +4,9 @@ import subprocess
 from bs4 import BeautifulSoup
 import json
 
+# Download the KMZ file and extract the KML inside
 response = requests.get(f"https://data.cityofchicago.org/download/rytz-fq6y/"
-                            "application%2Fvnd.google-earth.kmz")
-
+                        "application%2Fvnd.google-earth.kmz")
 with open('temp.kmz', 'wb') as f:
     f.write(response.content)
 kmz = zipfile.ZipFile('temp.kmz', 'r')
@@ -16,31 +16,44 @@ with open('temp.kml', 'wb') as f:
 
 # Use ogr2ogr to convert KML to GeoJSON
 subprocess.run(['ogr2ogr', '-f', 'GeoJSON', 'data/map.json', 'temp.kml'])
-
-# Load the GeoJSON file
 with open('data/map.json', 'r') as f:
     data = json.load(f)
 
-# Iterate over the features and clean the HTML from the 'description' field
+# Clean the HTML from the 'description' field
 for feature in data['features']:
     soup = BeautifulSoup(feature['properties']['description'], 'html.parser')
     feature['properties']['description'] = soup.get_text()
 
-    # Parse properties from the 'description' field
+    # After cleaning, each feature's 'properties' field looks like this
+    # For now, we want only the 'ROUTE' and 'NAME' properties (case-sensitive)
+    #  "Name": "111A",
+    #  "description": "a bunch of gibberish",
+    #  "altitudeMode": "clampToGround",
+    #  "tessellate": 1,
+    #  "extrude": 0,
+    #  "visibility": -1,
+    #  "snippet": "",
+    #  "ROUTE": "111A",
+    #  "ROUTE0": "111A",
+    #  "NAME": "PULLMAN SHUTTLE",
+    #  "WKDAY": "1",
+    #  "SAT": "1",
+    #  "SUN": "1",
+    #  "SHAPE.LEN": "19392.594684"
     for table in soup.find_all('table'):
         for row in table.find_all('tr'):
             cells = row.find_all('td')
             if len(cells) == 2:
                 prop = cells[0].get_text().strip()
                 value = cells[1].get_text().strip()
-                if prop in ["Name", "ROUTE", "NAME", "SHAPE.LEN"]:
+                if prop in ["ROUTE", "NAME"]:
                     feature['properties'][prop] = value
 
     # Remove unwanted properties
-    unwanted_props = [prop for prop in feature['properties'] if prop not in ["Name", "ROUTE", "NAME", "SHAPE.LEN"]]
+    unwanted_props = [prop for prop in feature['properties'] if prop not in ["ROUTE", "NAME"]]
     for prop in unwanted_props:
         del feature['properties'][prop]
 
-# Write the cleaned data back to the file
+# Save the cleaned GeoJSON data
 with open('data/map.json', 'w') as f:
     json.dump(data, f)
