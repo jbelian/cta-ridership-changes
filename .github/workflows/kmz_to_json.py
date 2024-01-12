e159b18e3f0c97d9e1be9347a46d6144fee5e20e
Joseph <33590262+jbelian@users.noreply.github.com>
2024-01-11 20:51:34 -0500

    update

--- .github/workflows/kmz_to_json.py
+++ .github/workflows/kmz_to_json.py
@@ -1,6 +1,7 @@
 import requests
 import zipfile
 from osgeo import ogr
+from bs4 import BeautifulSoup
 
 response = requests.get(f"https://data.cityofchicago.org/download/rytz-fq6y/"
                             "application%2Fvnd.google-earth.kmz")
@@ -9,8 +10,19 @@ with open('temp.kmz', 'wb') as f:
     f.write(response.content)
 kmz = zipfile.ZipFile('temp.kmz', 'r')
 kml = kmz.open('doc.kml', 'r').read()
+
+# Parse the KML with BeautifulSoup
+soup = BeautifulSoup(kml, 'lxml')
+
+# Find all the elements that contain HTML and remove them
+for tag in soup.find_all():
+    if tag.name not in ['kml', 'document', 'placemark', 'name', 'point', 'coordinates']:
+        tag.decompose()
+
+# Write the cleaned KML back to a file
 with open('temp.kml', 'wb') as f:
-    f.write(kml)
+    f.write(str(soup).encode())
+
 kml_driver = ogr.GetDriverByName('KML')
 kml_ds = kml_driver.Open('temp.kml')
 
