4c8401235790277ec58718d09e91831f1f2e6e0c
Joseph <33590262+jbelian@users.noreply.github.com>
2024-01-11 22:06:49 -0500

    update

--- .github/workflows/kmz_to_json.py
+++ .github/workflows/kmz_to_json.py
@@ -1,8 +1,6 @@
 import requests
 import zipfile
-from osgeo import ogr
-from bs4 import BeautifulSoup
-import json
+import subprocess
 
 response = requests.get(f"https://data.cityofchicago.org/download/rytz-fq6y/"
                             "application%2Fvnd.google-earth.kmz")
@@ -11,37 +9,8 @@ with open('temp.kmz', 'wb') as f:
     f.write(response.content)
 kmz = zipfile.ZipFile('temp.kmz', 'r')
 kml = kmz.open('doc.kml', 'r').read()
-
-# Parse the KML with BeautifulSoup
-soup = BeautifulSoup(kml, 'xml')
-
-# Find all the elements that contain HTML and remove them
-for tag in soup.find_all():
-    if tag.name not in ['kml', 'document', 'placemark', 'name', 'point', 'coordinates']:
-        tag.decompose()
-
-# Write the cleaned KML back to a file
 with open('temp.kml', 'wb') as f:
-    f.write(str(soup).encode())
+    f.write(kml)
 
-kml_driver = ogr.GetDriverByName('KML')
-kml_ds = kml_driver.Open('temp.kml')
-
-# Though GeoJSON, the file extension must be .json for later manipulation
-geojson_driver = ogr.GetDriverByName('GeoJSON')
-geojson_ds = geojson_driver.CopyDataSource(kml_ds, 'data/map.json')
-
-# Open the GeoJSON file and parse it
-with open('data/map.json', 'r') as f:
-    data = json.load(f)
-
-# Clean the HTML from the "Description" field
-for feature in data['features']:
-    description = feature['properties']['Description']
-    soup = BeautifulSoup(description, 'lxml')
-    cleaned_text = soup.get_text()
-    feature['properties']['Description'] = cleaned_text
-
-# Write the cleaned GeoJSON back to the file
-with open('data/map.json', 'w') as f:
-    json.dump(data, f)
+# Use ogr2ogr to convert KML to GeoJSON
+subprocess.run(['ogr2ogr', '-f', 'GeoJSON', 'data/map.json', 'temp.kml'])
