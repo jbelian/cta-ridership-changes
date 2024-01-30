08578d623fe41aff7a5891909187d40d4243bf10
Joseph <33590262+jbelian@users.noreply.github.com>
2024-01-29 23:46:59 -0500

    commented out function to pull rail line data

--- scripts/kmz_to_json.py
+++ scripts/kmz_to_json.py
@@ -34,20 +34,37 @@ def clean_properties(data, properties):
         # Replace the feature's properties with the extracted properties
         feature['properties'] = extracted_props
 
+def save_data(data, filename):
+    with open(filename, 'w') as f:
+        json.dump(data, f)
+
 # Call the functions with the provided URLs
 bus_map_data = kmz_to_json("https://data.cityofchicago.org/download/rytz-fq6y/"
                     "application%2Fvnd.google-earth.kmz", "output1.json")
-rail_map_data = kmz_to_json("https://data.cityofchicago.org/download/sgbp-qafc/"
-                    "application%2Fvnd.google-earth.kmz", "output2.json")
 station_map_data = kmz_to_json("https://data.cityofchicago.org/download/4qtv-9w43/"
-                           "application%2Fvnd.google-earth.kmz", "output3.json")
+                           "application%2Fvnd.google-earth.kmz", "output2.json")
+
+# Clean the HTML off the properties and extract the parameters specified
+clean_properties(bus_map_data, ['ROUTE', 'NAME'])
+clean_properties(station_map_data, ['Station ID', 'Station Name'])
+
+# Save the cleaned GeoJSON data
+save_data(bus_map_data, 'data/busMap.json')
+save_data(station_map_data, 'data/stationMap.json')
+
+# The rail map has been customized and must not be completely overwritten
+# However, to fetch it again, add "data/railMap.json" to "Stage map files" in access_api.yaml
+# rail_map_data = kmz_to_json("https://data.cityofchicago.org/download/sgbp-qafc/"
+#                     "application%2Fvnd.google-earth.kmz", "output3.json")
+# clean_properties(rail_map_data, ['LEGEND', 'ALT_LEGEND'])
+# save_data(rail_map_data, 'data/railMap.json')
 
 
-# After cleaning, each feature's full 'properties' field is shown below
-# That's unnecessary, as only the parameters specified are needed for now
+# After cleaning, an example of each feature's full 'properties' field is shown below
 
+# BUS PROPERTIES
 #  "Name": "111A",
-#  "description": "a bunch of gibberish",
+#  "description": "",
 #  "altitudeMode": "clampToGround",
 #  "tessellate": 1,
 #  "extrude": 0,
@@ -60,9 +77,24 @@ station_map_data = kmz_to_json("https://data.cityofchicago.org/download/4qtv-9w4
 #  "SAT": "1",
 #  "SUN": "1",
 #  "SHAPE.LEN": "19392.594684"
-clean_properties(bus_map_data, ['ROUTE', 'NAME'])
 
+# STATION PROPERTIES
+# "Name": "Clark/Lake",
+# "Station ID": "380",
+# "Station Name": "Clark/Lake",
+# "Rail Line": "Brown, Orange, Pink, Purple (Express), Green, Blue",
+# "Address": "100 W. Lake",
+# "ADA": "ADA Accessible",
+# "Park and Ride": "No",
+# "POINT_X": "1175525.859134",
+# "POINT_Y": "1901735.36642",
+# "altitudeMode": "clampToGround",
+# "tessellate": -1,
+# "extrude": 0,
+# "visibility": -1,
+# "snippet": ""
 
+# RAIL PROPERTIES
 # "Name": "Brown, Green, Orange, Pink, Purple (Exp)",
 # "LINES": "Brown, Green, Orange, Pink, Purple (Exp)",
 # "DESCRIPTIO": "Clark/Lake to State/Lake",
@@ -78,28 +110,3 @@ clean_properties(bus_map_data, ['ROUTE', 'NAME'])
 # "extrude": 0,
 # "visibility": -1,
 # "snippet": ""
-clean_properties(rail_map_data, ['LEGEND', 'ALT_LEGEND'])
-
-# "Name": "Clark/Lake",
-# "Station ID": "380",
-# "Station Name": "Clark/Lake",
-# "Rail Line": "Brown, Orange, Pink, Purple (Express), Green, Blue",
-# "Address": "100 W. Lake",
-# "ADA": "ADA Accessible",
-# "Park and Ride": "No",
-# "POINT_X": "1175525.859134",
-# "POINT_Y": "1901735.36642",
-# "altitudeMode": "clampToGround",
-# "tessellate": -1,
-# "extrude": 0,
-# "visibility": -1,
-# "snippet": ""
-clean_properties(station_map_data, ['Station ID', 'Station Name'])
-
-# Save the cleaned GeoJSON data
-with open('data/busMap.json', 'w') as f:
-    json.dump(bus_map_data, f)
-with open('data/railMap.json', 'w') as f:
-    json.dump(rail_map_data, f)
-with open('data/stationMap.json', 'w') as f:
-    json.dump(station_map_data, f)
