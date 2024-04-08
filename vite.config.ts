04f0db1b4528c269f050dc92f938103f37568790
Joseph <33590262+jbelian@users.noreply.github.com>
2024-04-08 18:58:26 -0400

    added aliases for src and utils

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/cta-ridership-changes'
-})
+  base: "/cta-ridership-changes",
+  resolve: {
+    alias: {
+      "@": "/src",
+      "@utils": "/utils",
+    },
+  },
+});
