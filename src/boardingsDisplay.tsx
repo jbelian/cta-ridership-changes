39a5d7eff25c63d9592a0cb56d1a2239dfcb00ab
Joseph <33590262+jbelian@users.noreply.github.com>
2024-04-13 22:19:15 -0400

    updated styling

--- src/boardingsDisplay.tsx
+++ src/boardingsDisplay.tsx
@@ -1,4 +1,4 @@
-import { CombinedBoardings } from "../utils/dataHandlers.tsx";
+import { CombinedBoardings } from "./utils/dataHandlers.tsx";
 
 const getYearAndMonthName = (dateString: string) => {
   const date = new Date(`${dateString}T00:00:00`);
@@ -21,21 +21,23 @@ function BoardingsDisplay({
   toggle: boolean;
 }) {
   return (
-    <div>
-      <table className="table-fixed w-full">
+    <div className="pb-5 pl-5 pr-5">
+      <table className="w-full table-fixed">
         <thead>
-          <tr>
-            {toggle && <th className="w-[55px] text-left">Route</th>}
-            <th className="w-auto text-left truncate">
-              {toggle ? "Bus" : "Train Station"}
+          <tr className="sticky top-[70px] z-10 bg-background">
+            {toggle && (
+              <th className="w-[55px] pb-2 text-left font-semibold">Route</th>
+            )}
+            <th className="w-auto truncate pb-2 text-left font-semibold">
+              {toggle ? "Bus" : '"L" Station'}
             </th>
-            <th className="w-[77px] text-right">
+            <th className="w-[77px] pb-2 text-right font-semibold">
               {getYearAndMonthName(selectedDate1)}
             </th>
-            <th className="w-[77px] text-right">
+            <th className="w-[83px] pb-2 text-right font-semibold">
               {getYearAndMonthName(selectedDate2)}
             </th>
-            <th className="w-[67px] text-right">Change</th>
+            <th className="w-[67px] pb-2 text-right font-semibold">Change</th>
           </tr>
         </thead>
         <tbody>
@@ -44,15 +46,15 @@ function BoardingsDisplay({
               {toggle && <td className="w-[55px] text-left">{item.id}</td>}
               <td className="w-auto text-left">
                 <div
-                  className="whitespace-nowrap overflow-hidden overflow-ellipsis max-w-full"
+                  className="max-w-full overflow-hidden overflow-ellipsis whitespace-nowrap"
                   title={item.name}
                 >
                   {item.name}
                 </div>
               </td>
-              <td className="w-[77px] text-right">{item.monthTotal}</td>
-              <td className="w-[77px] text-right">{item.monthTotal2}</td>
-              <td className="w-[67px] text-right">{item.percentChange}</td>
+              <td className="text-right">{item.monthTotal}</td>
+              <td className="text-right">{item.monthTotal2}</td>
+              <td className="text-right">{item.percentChange}</td>
             </tr>
           ))}
         </tbody>
